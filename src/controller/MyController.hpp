44fab559cd4fcdc0cd9d11b896e201a075f928e5
Eren Kara <erenkyi0@gmail.com>
2020-12-30 18:30:26 +0300

    added jsonDb and a test usage

--- src/controller/MyController.hpp
+++ src/controller/MyController.hpp
@@ -1,11 +1,17 @@
 #ifndef MyController_hpp
 #define MyController_hpp
 
+#include <string>
+
 #include "algorithm/test.hpp"
+#include "db/jsonDb.hpp"
 #include "dto/DTOs.hpp"
 #include "oatpp/core/macro/codegen.hpp"
 #include "oatpp/core/macro/component.hpp"
 #include "oatpp/web/server/api/ApiController.hpp"
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 
 #include OATPP_CODEGEN_BEGIN(ApiController)  //<-- Begin Codegen
 
@@ -34,6 +40,38 @@ class MyController : public oatpp::web::server::api::ApiController {
         auto dto = TestDTO::createShared();
         dto->statusCode = 200;
         dto->html = "<b>test</b>";
+        return createDtoResponse(Status::CODE_200, dto);
+    }
+
+    ENDPOINT_INFO(jsontest) {
+        info->summary = "test json stuff";
+    }
+    ENDPOINT("GET", "testjson", jsontest) {
+        JsonDB db("../data/test.json");
+        const std::string dbContent = db.readFile("{\"project\":\"MRE\",\"dbType\":\"json\",\"count\": 0}");
+
+        // 1. Parse a JSON string into DOM.
+        rapidjson::Document d;
+        d.Parse(dbContent.c_str());
+
+        // 2. Modify it by DOM.
+        rapidjson::Value& dbType = d["dbType"];
+        dbType.SetString("JSON");
+        
+        rapidjson::Value& count = d["count"];
+        count.SetInt(count.GetInt() + 1);
+
+        // 3. Stringify the DOM
+        rapidjson::StringBuffer buffer;
+        rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+        d.Accept(writer);
+
+        // Output
+        db.writeFile(buffer.GetString());
+
+        auto dto = TestDTO::createShared();
+        dto->statusCode = 200;
+        dto->html = "done";
         return createDtoResponse(Status::CODE_200, dto);
     }
 
