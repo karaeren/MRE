edaf3c36c431ae7fa99968db65640dcb2272cba7
Eren Kara <erenkyi0@gmail.com>
2021-01-17 16:38:23 +0300

    added frontend server and built files

---
+++ frontend/server.js
@@ -0,0 +1,18 @@
+var fs = require("fs"),
+  http = require("http");
+
+http
+  .createServer(function (req, res) {
+    fs.readFile(__dirname + "/public" + req.url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  })
+  .listen(8282);
+
+console.log("frontend running");
