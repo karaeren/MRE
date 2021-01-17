6c5f62da889cc04f2d5e3c92f667acf0ee7a276b
sirmaci <54437837+sirmaci@users.noreply.github.com>
2021-01-18 00:36:02 +0300

    Algorithm_comment1

--- src/algorithm/similarity.cpp
+++ src/algorithm/similarity.cpp
@@ -1,13 +1,18 @@
 #include "similarity.hpp"
 
+
+
+//Returns a distance-based similarity score for person1 and person2
+//First param is movie critic container (type of heap)
 float Similarity::sim_distance(std::unordered_map<std::string, std::unordered_map<std::string, float>> prefs, std::string person1, std::string person2) {
-    std::unordered_map<std::string, float> si;
+    std::unordered_map<std::string, float> si;  //Heap of shared_items
     std::unordered_map<std::string, float> items_person1;
     std::unordered_map<std::string, float> items_person2;
 
     items_person1 = prefs[person1];
     items_person2 = prefs[person2];
-
+    
+    //Get the list of shared_items and add them to "si" heap
     for (auto x : items_person1) {
         //x.first = film adi
         //x.second = rank
@@ -18,6 +23,8 @@ float Similarity::sim_distance(std::unordered_map<std::string, std::unordered_ma
         }
     }
 
+    
+    //if they have no ratings in common, returns 0
     int si_counter = 0;
     for (auto i : si) {
         si_counter += 1;
@@ -26,6 +33,8 @@ float Similarity::sim_distance(std::unordered_map<std::string, std::unordered_ma
         return 0.0;
     }
 
+    
+    //Add up the squares of all the differences
     float sum_of_squares = 0;
     for (auto x : si) {
         std::string item = x.first;
@@ -33,16 +42,22 @@ float Similarity::sim_distance(std::unordered_map<std::string, std::unordered_ma
         sum_of_squares += pow(farklar, 2.0);
     }
     return 1 / (1 + sqrt(sum_of_squares));
+    
 }
-//
+
+
+//Returns the Pearson correlation coefficient for p1 and p2
+//First param is movie critic container (type of heap)
 float Similarity::sim_pearson(std::unordered_map<std::string, std::unordered_map<std::string, float>> prefs, std::string person1, std::string person2) {
-    std::unordered_map<std::string, float> si;
+    std::unordered_map<std::string, float> si; // Heap of mutually rated items
     std::unordered_map<std::string, float> items_person1;
     std::unordered_map<std::string, float> items_person2;
 
     items_person1 = prefs[person1];
     items_person2 = prefs[person2];
 
+    
+    //Get the list of mutually rated items
     for (auto x : items_person1) {
         //x.first = film adi
         //x.second = rank
@@ -53,7 +68,9 @@ float Similarity::sim_pearson(std::unordered_map<std::string, std::unordered_map
         }
     }
 
-    int si_counter = 0;
+    
+    //if they are no ratings in common, return 0
+    int si_counter = 0; //Sum calculations
     for (auto i : si) {
         si_counter += 1;
     }
@@ -61,6 +78,9 @@ float Similarity::sim_pearson(std::unordered_map<std::string, std::unordered_map
         return 0.0;
     }
 
+    
+    //sum1 and sum2 variable represent the sums of all the preferences
+    //sum1Sq and sum2Sq variable represent the Sums of the squares
     float sum1 = 0, sum1Sq = 0;
     float sum2 = 0, sum2Sq = 0;
     float pSum = 0;
@@ -70,7 +90,8 @@ float Similarity::sim_pearson(std::unordered_map<std::string, std::unordered_map
 
         sum2 += prefs[person2][puanlar.first];
         sum2Sq += pow(prefs[person2][puanlar.first], 2.0);
-
+        
+        //Sum of the products
         pSum += prefs[person2][puanlar.first] * prefs[person1][puanlar.first];
     }
 
